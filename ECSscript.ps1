0877749dff0249aaa8d3749c763889edaee3d52c
Neyo li <thynei2021@gmail.com>
2023-05-26 11:36:16 +0800

    ecs script1

--- ECSscript.ps1
+++ ECSscript.ps1
@@ -15,12 +15,12 @@ docker push 802783396857.dkr.ecr.eu-west-1.amazonaws.com/nei_repository:latest
 
 
 $port = 3000
-$name = "app03"
+$name = "cicd-myapp"
 $image = "802783396857.dkr.ecr.eu-west-1.amazonaws.com/nei_repository:latest"
 
 # Define Container
 $PortMappings = New-Object "System.Collections.Generic.List[Amazon.ECS.Model.PortMapping]"
-$PortMappings.Add($(New-Object -TypeName "Amazon.ECS.Model.PortMapping" -Property @{ HostPort=$port; ContainerPort=$port; Protocol=[Amazon.ECS.TransportProtocol]::Tcp}))
+$PortMappings.Add($(New-Object -TypeName "Amazon.ECS.Model.PortMapping" -Property @{ HostPort = $port; ContainerPort = $port; Protocol = [Amazon.ECS.TransportProtocol]::Tcp }))
 $PortMappings
 
 $EnvironmentVariables = New-Object "System.Collections.Generic.List[Amazon.ECS.Model.KeyValuePair]"
@@ -30,18 +30,19 @@ $EnvironmentVariables = New-Object "System.Collections.Generic.List[Amazon.ECS.M
 Write-Host "Adding Container Definition for" $image
 $ContainerDefinitions = New-Object "System.Collections.Generic.List[Amazon.ECS.Model.ContainerDefinition]"
 $ContainerDefinitions.Add($(New-Object -TypeName "Amazon.ECS.Model.ContainerDefinition" -Property @{ `
-    Name=$name; `
-    Image=$image; `
-    PortMappings=$PortMappings; `
-    Environment=$EnvironmentVariables
-    Memory=256;}))
+                Name         = $name; `
+                Image        = $image; `
+                PortMappings = $PortMappings; `
+                Environment  = $EnvironmentVariables
+            Memory           = 256
+        }))
 
 # Create Task
 $Region = "eu-west-1"
-$TaskName = "task03"
+$TaskName = "cicd-task"
 $ExecutionRole = $(Get-IAMRole -profilename 8027 -RoleName "ecsTaskExecutionROle").Arn
 Write-Host "Creating New Task Definition $TaskName"
-$TaskDefinition = Register-ECSTaskDefinition  -profilename 8027 `
+$TaskDefinition = Register-ECSTaskDefinition -profilename 8027 `
     -ContainerDefinition $ContainerDefinitions `
     -Cpu 256 `
     -Family $TaskName `
@@ -57,16 +58,16 @@ Write-Verbose $TaskDefinition | ConvertTo-Json
 
 
 # if no service, create new ecs service through aws-cli cloudformation
-aws cloudformation deploy --template-file ./web3.json --stack-name Stack02 --parameter-overrides InstanceTypeParameter=t2.micro --profile 8027 --region eu-west-1
+aws cloudformation deploy --template-file ./web3.json --stack-name cicd-stack --parameter-overrides InstanceTypeParameter=t2.micro --profile 8027 --region eu-west-1
 
 # Get last task
-$lastECSTaskDefinitions = Get-ECSTaskDefinitions -ProfileName 8027 -Region eu-west-1 |select -Last 1 
+$lastECSTaskDefinitions = Get-ECSTaskDefinitions -ProfileName 8027 -Region eu-west-1 | Select-Object -Last 1 
 $lastECSTaskDefinitions
-$LastTaskDefinition = Get-ECSTaskDefinitionDetail -ProfileName 8027 -Region eu-west-1  -TaskDefinition task03
+$LastTaskDefinition = Get-ECSTaskDefinitionDetail -ProfileName 8027 -Region eu-west-1 -TaskDefinition cicd-task:1
 
 # Update Service
-$ClusterName = "cu2"
-$ServiceName = "service02"
+$ClusterName = "cicd-cluster"
+$ServiceName = "cicd-service"
 Write-Host "Updating Service $ServiceName"
 $ServiceUpdate = Update-ECSService -profilename 8027 -Region $Region `
     -Cluster $ClusterName `
